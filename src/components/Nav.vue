04ebf9f1b0ca1fbdac8155d8846c1b8f3c8b9ef2
Qi Xiang <qixyqix@outlook.com>
2020-01-26 21:58:58 +0800

    Added toggleability to hamburger button

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,15 +2,18 @@
   <nav class="flex items-center justify-between flex-wrap bg-primary">
     <div class="container flex flex-col md:flex-row font-bold text-white text-lg">
       <div class="block lg:hidden">
-        <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+        <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+        @click="showlinks = !showlinks">
           <svg class="fill-current h-3 w-3" viewBox="0 0 20 20">
             <title>Menu</title>
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
           </svg>
         </button>
       </div>
+      <div v-if="showlinks">
       <div v-for="link in links" v-bind:key="link.name">
           <router-link  class="mr-4 hover:text-green-200" :to="{name: link.name}">{{link.display}}</router-link>
+      </div>
       </div>
     </div>
   </nav>
@@ -25,7 +28,8 @@ export default {
           {name: "page.causes",display:"Causes"},
           {name: "page.prevention",display:"Prevention"},
           {name: "page.sources",display:"Sources"},
-          {name: "page.countries",display:"Countries"}]
+          {name: "page.countries",display:"Countries"}],
+          showlinks: true
       }
   }
 };
