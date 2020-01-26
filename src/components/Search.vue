8d0d7d80e59044b43ea58254ef189403d1c738d0
Hexareus <dashhax@gmail.com>
2020-01-26 17:14:08 +0800

    Started with search function

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center">
-    <input type="text" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200  py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-300" />
-    <button class="bg-primary h-12 w-12 flex items-center justify-center">
+    <input v-on:input="typedSearch" v-model="term" type="text" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200  py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-300" />
+    <button class="bg-primary h-12 w-12 flex items-center justify-center" v-on:click="search">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="23"
@@ -22,8 +22,33 @@
 </template>
 
 <script>
+
+
 export default {
-  name: "Search"
+  name: "Search",
+  data: function () {
+    return {
+      term: "",
+      timeoutID: null,
+
+    }
+  },
+  methods: {
+    performSearch: function () {
+      // eslint-disable-next-line no-console
+      console.log(this.term);
+    },
+    typedSearch: function () {
+      clearTimeout(this.timeoutID);
+      
+      this.timeoutID = setTimeout(() => {
+        this.performSearch();
+      }, 250);
+    },
+    search: function () {
+      this.performSearch();
+    }
+  }
 };
 </script>
 
