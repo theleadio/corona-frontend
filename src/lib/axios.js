9c8bede7c569a8905085db1240428dc4b2238c35
Wei Seng Tan <tanweiseng13@gmail.com>
2020-01-26 21:29:26 +0800

    Set default API_BASE_URL for local development

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.VUE_APP_API_URL;
+const API_BASE_URL = process.env.VUE_APP_API_URL || 'http://localhost:3000';
 
 const request = axios.create({
     baseURL: API_BASE_URL,
