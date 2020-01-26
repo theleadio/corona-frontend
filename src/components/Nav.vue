e5b5d34c92b6c084264ab58362cbb578653dc6c2
Qi Xiang <qixyqix@outlook.com>
2020-01-26 22:15:01 +0800

    Fixed menu layout on desktop

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,7 +2,7 @@
   <nav class="flex items-center justify-between flex-wrap bg-primary">
     <div class="container flex flex-col md:flex-row font-bold text-white text-lg">
       <div class="block lg:hidden">
-        <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+        <button id="hamburger" class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
         @click="showlinks = !showlinks">
           <svg class="fill-current h-3 w-3" viewBox="0 0 20 20">
             <title>Menu</title>
@@ -10,10 +10,8 @@
           </svg>
         </button>
       </div>
-      <div v-if="showlinks">
       <div v-for="link in links" v-bind:key="link.name">
-          <router-link  class="mr-4 hover:text-green-200" :to="{name: link.name}">{{link.display}}</router-link>
-      </div>
+          <router-link v-if="showlinks" class="mr-4 hover:text-green-200" :to="{name: link.name}">{{link.display}}</router-link>
       </div>
     </div>
   </nav>
