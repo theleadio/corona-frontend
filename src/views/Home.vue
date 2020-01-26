26e6bbcaa53a9f63359a9c571e32a340794a8334
Wei Seng Tan <tanweiseng13@gmail.com>
2020-01-26 23:12:09 +0800

    Implement pagination for trending news

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,7 @@
       <div class="flex flex-wrap -mx-2">
         
         <div class="w-full md:w-2/3 px-2">
-          <Card title="Trending News"> </Card>
-  
           <TrendingNews></TrendingNews>
-
         </div>
         <div class="w-full md:w-1/3 px-2">
           <div class="mb-4">
@@ -22,7 +19,6 @@
   </div>
 </template>
 <script>
-import Card from "../components/Card";
 import NotifyCard from "../components/NotifyCard";
 import RecentNews from "../components/RecentNews";
 import Search from "../components/Search";
@@ -30,7 +26,6 @@ import TrendingNews from './Home/TrendingNews';
 
 export default {
   components: {
-    Card,
     NotifyCard,
     RecentNews,
     Search,
