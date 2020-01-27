efd3a59e20a4c6b93b52642e0cb52e6bf0812812
Qi Xiang <qixyqix@outlook.com>
2020-01-27 12:14:55 +0800

    Fixed formatting for countryselector & base statistics

--- src/components/LocationSelector.vue
+++ src/components/LocationSelector.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="bg-white rounded border-solid border-2 border-primary p-6 flex justify-between">
+  <div class="bg-white rounded border-solid border-2 border-primary p-6 flex flex-wrap">
 
-    <div>
+    <div class="w-full md:w-1/2">
       <label
         class="block tracking-wide text-gray-700 text-s font-bold mb-2"
         for="select-country">I am from...</label>
       <div>
-        <select class="block appearance-none bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+        <select class="block w-full md:w-1/2 appearance-none bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
           id="select-country">
-          <option>Malaysia</option>
+          <option><span class="flag-icon flag-icon-gr"></span> Malaysia</option>
           <option>Singapore</option>
           <option>Vietnam</option>
           <option>Thailand</option>
@@ -16,26 +16,26 @@
       </div>
     </div>
 
-    <div class="flex">
-        <div class="text-center pl-5 pr-5">
-            <div class="text-primary">
-              <span class="text-5xl">4</span>
+    <div class="flex justify-center w-full md:justify-end md:w-1/2 mt-5 md:mt-0">
+        <div class="text-center pl-2 pr-2 md:pl-5 md:pr-5">
+            <div class="text-primary text-3xl md:text-5xl">
+              4
             </div>
             <div class="text-sm uppercase text-grey tracking-wide">
               Confirmed
             </div>
         </div>
-        <div class="text-center pl-5 pr-5">
-            <div class="text-primary">
-              <span class="text-5xl">42</span>
+        <div class="text-center pl-2 pr-2 md:pl-5 md:pr-5">
+            <div class="text-primary text-3xl md:text-5xl">
+              42
             </div>
             <div class="text-sm uppercase text-grey tracking-wide">
               Pending
             </div>
         </div>
-        <div class="text-center pl-5 pr-5">
-            <div class="text-primary">
-              <span class="text-5xl">46</span>
+        <div class="text-center pl-2 pr-2 md:pl-5 md:pr-5">
+            <div class="text-primary text-3xl md:text-5xl">
+              46
             </div>
             <div class="text-sm uppercase text-grey tracking-wide">
               Negative
