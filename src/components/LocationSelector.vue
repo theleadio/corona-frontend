9f3a25f3ee4dfeead647ad67d31436e2e5877c75
Wei Seng Tan <tanweiseng13@gmail.com>
2020-01-28 02:14:43 +0800

    Add more countries to selection

--- src/components/LocationSelector.vue
+++ src/components/LocationSelector.vue
@@ -92,7 +92,12 @@ export default {
       showOptions: false,
       global: {code: "global", name: "Global"},
       countries: [
+        { code: "cn", name: "China" },
+        { code: "id", name: "Indonesia" },
+        { code: "jp", name: "Japan" },
+        { code: "kr", name: "South Korea" },
         { code: "my", name: "Malaysia" },
+        { code: "ph", name: "Philippines" },
         { code: "sg", name: "Singapore" },
         { code: "th", name: "Thailand" },
         { code: "vn", name: "Vietnam" },
