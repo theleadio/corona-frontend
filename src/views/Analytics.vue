445c8b253530dac4426b23ec1e222c42d27b4e25
Dr. Lau Cher Han <laucherhan@gmail.com>
2020-01-27 12:30:17 +0800

    updated analytics

--- src/views/Analytics.vue
+++ src/views/Analytics.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container">
-    <h1>Analytics</h1>
-    <div>Work in progress...</div>
+    <div class='w-full h-screen'>
+      <iframe width="100%" height="800" src="https://datastudio.google.com/embed/reporting/c0771868-a928-4308-bf5d-e3af39567379/page/1M" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
   </div>
 </template>
 <script>
