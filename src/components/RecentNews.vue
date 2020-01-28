aeed11d2d08f459f8f4b96aba4762b8bd180b4e7
Zarul <zarulizham97@gmail.com>
2020-01-28 14:55:21 +0800

    Update skeleton loading

--- src/components/RecentNews.vue
+++ src/components/RecentNews.vue
@@ -1,41 +1,46 @@
 <template>
   <div>
     <Card title="Recent News"> </Card>
-    <a
-      v-for="news in recentNews" :key="news.id"
-      :href="news.url"
-      target="_blank"
-      class="bg-gray-100 p-3 text-sm block"
-    >
-      <p class="leading-normal"><span class="font-bold">{{ news.title }}</span> - {{ news.description }}</p>
-      <p class="text-right font-semibold">{{ news.publishedAt | moment('from') }}</p>
-    </a>
+    <Loading :ajax="ajax">
+      <a
+        v-for="news in recentNews" :key="news.id"
+        :href="news.url"
+        target="_blank"
+        class="bg-gray-100 p-3 text-sm block"
+      >
+        <p class="leading-normal"><span class="font-bold">{{ news.title }}</span> - {{ news.description }}</p>
+        <p class="text-right font-semibold">{{ news.publishedAt | moment('from') }}</p>
+      </a>
+    </Loading>
   </div>
 </template>
 
 <script>
 import Card from "../components/Card";
+import Loading from '../components/Loading';
 import { getRecentNews } from "../api/news";
 
 export default {
   name: "RecentNews",
   components: {
     Card,
+    Loading
   },
   data() {
     return {
       recentNews: [],
+      ajax: null
     };
   },
   created() {
-    getRecentNews()
+    this.ajax = getRecentNews()
       .then(data => {
         this.recentNews = data;
       })
-      .catch(err => {
-        // eslint-disable-next-line no-console
-        console.log(err);
-      });
+      // .catch(err => {
+      //   // eslint-disable-next-line no-console
+      //   console.log(err);
+      // });
   }
 }
 </script>
