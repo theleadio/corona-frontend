0dd25d95fe5accc7786e9a9b36e23211d1851d18
Wei Seng Tan <tanweiseng13@gmail.com>
2020-01-30 22:35:17 +0800

    Update getTrendingNews API to filter by language

--- src/api/news.js
+++ src/api/news.js
@@ -5,7 +5,7 @@ export const getRecentNews = ({ limit = 4, sort = '-publishedAt', country = '' }
     .then(res => getData(res));
 };
 
-export const getTrendingNews = ({ limit, offset, country = '' }) => {
-  return axios.get(`/news/trending?offset=${offset}&limit=${limit}&country=${country}`)
+export const getTrendingNews = ({ limit, offset, country = '', language = 'en' }) => {
+  return axios.get(`/news/trending?offset=${offset}&limit=${limit}&country=${country}&language=${language}`)
     .then(res => getData(res));
 };
