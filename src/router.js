2d8de4d56061514c465dae24ee616223ea7ee5a5
Deojeff Lai <d30jeff@gmail.com>
2020-01-30 23:15:40 +0800

    Fix # appearing in production URL

--- src/router.js
+++ src/router.js
@@ -1,49 +1,46 @@
-import Vue from "vue";
-import Router from "vue-router";
-
-import Home from "../src/views/Home";
-import Causes from "../src/views/Causes";
-import Prevention from "../src/views/Prevention";
-import Sources from "../src/views/Sources";
-import Analytics from "../src/views/Analytics";
-import About from "../src/views/About";
+import Vue from 'vue';
+import Router from 'vue-router';
+import About from '../src/views/About';
+import Analytics from '../src/views/Analytics';
+import Causes from '../src/views/Causes';
+import Home from '../src/views/Home';
+import Prevention from '../src/views/Prevention';
+import Sources from '../src/views/Sources';
 
 Vue.use(Router);
 
 export default new Router({
-    mode: "hash",
-    routes: [
-        {
-            path: "/",
-            component: Home,
-            name: 'page.home'
-        },
-        {
-            path: "/causes",
-            component: Causes,
-            name: 'page.causes',
-        },
-        {
-            path: "/prevention",
-            component: Prevention,
-            name: 'page.prevention',
-        },
-        {
-            path: "/sources",
-            component: Sources,
-            name: 'page.sources',
-        },
-        {
-            path: "/analytics",
-            component: Analytics,
-            name: 'page.analytics',
-        },
-        {
-            path: "/about",
-            component: About,
-            name: 'page.about',
-        },
-
-    ]
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: Home,
+      name: 'page.home'
+    },
+    {
+      path: '/causes',
+      component: Causes,
+      name: 'page.causes'
+    },
+    {
+      path: '/prevention',
+      component: Prevention,
+      name: 'page.prevention'
+    },
+    {
+      path: '/sources',
+      component: Sources,
+      name: 'page.sources'
+    },
+    {
+      path: '/analytics',
+      component: Analytics,
+      name: 'page.analytics'
+    },
+    {
+      path: '/about',
+      component: About,
+      name: 'page.about'
+    }
+  ]
 });
-
