a8d2230a3984b08a0eeacd1782235e41ca7c60fa
Wei Seng Tan <tanweiseng13@gmail.com>
2020-02-04 03:49:52 +0800

    Clean up

--- src/api/analytics.js
+++
@@ -1,6 +0,0 @@
-import axios, { getData } from '../lib/axios';
-
-export const fetchTrendByDate = (start_date, end_date) => {
-  return axios.get(`/analytics/trend?start_date=${start_date}&end_date=${end_date}`)
-    .then(res => getData(res))
-}
--- src/api/stats.js
+++
@@ -1,6 +0,0 @@
-import axios, { getData } from '../lib/axios';
-
-export const getStats = (country) => {
-  return axios.get(`/stats?country=${country}`)
-    .then(res => getData(res));
-};
