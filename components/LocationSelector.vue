ac7552b781fc8d7b5405bf72454391639176b040
emperorinfinite <emperorpointguard@gmail.com>
2020-02-05 07:19:14 +0800

    style homepage stats

--- components/LocationSelector.vue
+++ components/LocationSelector.vue
@@ -1,83 +1,55 @@
 <template>
-  <div class="bg-white rounded border-solid border-2 border-primary p-6 flex flex-wrap">
-    <div class="w-full md:w-1/2">
-      <label class="block tracking-wide text-gray-700 text-s font-bold mb-2" for="select-country">
-        I am from...
-      </label>
-      <div class="relative">
-        <button class="bg-gray-200 text-left font-bold py-2 px-4 rounded w-full md:w-1/2 flex" @click="toggleOptions" v-on-clickaway="closeOptions">
-          <div v-if="currentCountry && currentCountry.code === 'global'">
-            <i class="fas fa-globe"></i>
-            {{currentCountry.name}}
-          </div>
-          <div v-else-if="currentCountry">
-            <span :class="'cursor-pointer flag-icon flag-icon-'+currentCountry.code"></span>
-            {{currentCountry.name}}
-          </div>
-          <div v-else>Select Country</div>
-          <div class="self-center ml-auto">
-            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-            </svg>
-          </div>
-        </button>
-
-        <ul class="absolute text-gray-700 pt-1 z-50 w-full md:w-1/2" v-if="optionsShowed">
-          <li>
-            <a class="cursor-pointer bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" @click="selectCountry(global)">
+  <div class="bg-white rounded border-2 border-gray-200 p-4">
+    <div class="flex flex-wrap md:flex-no-wrap md:flex-row">
+      <div class="w-full md:w-1/2">
+        <label class="block tracking-wide text-gray-700 text-s font-bold mb-2" for="select-country">
+          I am from...
+        </label>
+        <div class="relative">
+          <button class="bg-gray-200 text-left font-bold py-2 px-4 rounded w-full md:w-1/2 flex" @click="toggleOptions" v-on-clickaway="closeOptions">
+            <div v-if="currentCountry && currentCountry.code === 'global'">
               <i class="fas fa-globe"></i>
-              <span class="ml-2">Global</span>
-            </a>
-          </li>
-          <li v-for="country in countries" v-bind:key="country.code">
-            <a class="cursor-pointer bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" @click="selectCountry(country)">
-              <span :class="'flag-icon flag-icon-' + country.code"></span>
-              <span class="ml-2">{{country.name}}</span>
-            </a>
-          </li>
-        </ul>
+              {{currentCountry.name}}
+            </div>
+            <div v-else-if="currentCountry">
+              <span :class="'cursor-pointer flag-icon flag-icon-'+currentCountry.code"></span>
+              {{currentCountry.name}}
+            </div>
+            <div v-else>Select Country</div>
+            <div class="self-center ml-auto">
+              <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+              </svg>
+            </div>
+          </button>
+
+          <ul class="absolute text-gray-700 pt-1 z-50 w-full md:w-1/2" v-if="optionsShowed">
+            <li>
+              <a class="cursor-pointer bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" @click="selectCountry(global)">
+                <i class="fas fa-globe"></i>
+                <span class="ml-2">Global</span>
+              </a>
+            </li>
+            <li v-for="country in countries" v-bind:key="country.code">
+              <a class="cursor-pointer bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" @click="selectCountry(country)">
+                <span :class="'flag-icon flag-icon-' + country.code"></span>
+                <span class="ml-2">{{country.name}}</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
+
+      <stats class="flex justify-center w-full md:justify-end mt-5 md:mt-0" :confirmed=numConfirm :recovered=numHeal :deaths=numDeath />
+
     </div>
 
-     <div class="flex justify-center w-full md:justify-end md:w-1/2 mt-5 md:mt-0">
-      <div class="text-center pl-2 pr-2 md:pl-5 md:pr-5">
-        <div class="text-primary text-3xl md:text-5xl">
-          <template v-if="typeof numConfirm === 'number'">
-            <animated-number :value="numConfirm" :round="1" :duration="300" />
-          </template>
-          <template v-else>
-            {{ numConfirm }}
-          </template>
-        </div>
-        <div class="text-sm uppercase text-grey tracking-wide">Confirmed</div>
-      </div>
-       <div class="text-center pl-2 pr-2 md:pl-5 md:pr-5">
-         <div class="text-primary text-3xl md:text-5xl">
-           <template v-if="typeof numHeal === 'number'">
-             <animated-number :value="numHeal" :round="1" :duration="300" />
-           </template>
-           <template v-else>
-             {{ numHeal }}
-           </template>
-         </div>
-         <div class="text-sm uppercase text-grey tracking-wide">Recovered</div>
-       </div>
-      <div class="text-center pl-2 pr-2 md:pl-5 md:pr-5">
-        <div class="text-primary text-3xl md:text-5xl">
-          <template v-if="typeof numDeath === 'number'">
-            <animated-number :value="numDeath" :round="1" :duration="300" />
-          </template>
-          <template v-else>
-            {{ numDeath }}
-          </template>
-        </div>
-        <div class="text-sm uppercase text-grey tracking-wide">Death</div>
-      </div>
-    </div>
+    <nuxt-link class="block text-right mt-6 underline text-blue-500 font-semibold" to="analytics">more details</nuxt-link>
   </div>
 </template>
 
 <script>
+import Stats from '~/components/Analytics/Stats';
 import AnimatedNumber from "animated-number-vue";
 import { directive as onClickaway } from 'vue-clickaway';
 
@@ -88,6 +60,7 @@ export default {
   },
   components: {
     AnimatedNumber,
+    Stats
   },
   data: function() {
     return {
@@ -105,9 +78,9 @@ export default {
         { code: "th", name: "Thailand" },
         { code: "vn", name: "Vietnam" },
       ],
-      numDeath: '',
-      numConfirm: '',
-      numHeal: '',
+      numDeath: 0,
+      numConfirm: 0,
+      numHeal: 0,
     };
   },
   methods: {
