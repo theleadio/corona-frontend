26e775ec21f1b7f1adbb42bf65d5c7434d12e243
Wei Seng Tan <tanweiseng13@gmail.com>
2020-02-04 20:50:44 +0800

    fix(layouts/default): fix warning

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,17 +5,19 @@
     </div>
     <Nav/>
     <nuxt/>
-    <back-to-top bottom="16px" right="16px" style="position: fixed;">
-      <button type="button" class="btn btn-info btn-to-top">
-        <svg width="24" height="24" viewBox="0 -256 1792 1792">
-          <g transform="matrix(1,0,0,-1,68.338983,1217.0847)">
-            <path
-                d="m 1611,320 q 0,-53 -37,-90 l -75,-75 q -38,-38 -91,-38 -54,0 -90,38 L 832,640 346,155 q -36,-38 -90,-38 -54,0 -90,38 l -75,75 q -38,36 -38,90 0,53 38,91 l 651,651 q 37,37 90,37 52,0 91,-37 l 650,-651 q 38,-38 38,-91 z"
-                style="fill:#fff"/>
-          </g>
-        </svg>
-      </button>
-    </back-to-top>
+    <client-only>
+      <back-to-top bottom="16px" right="16px">
+        <button type="button" class="btn btn-info btn-to-top">
+          <svg width="24" height="24" viewBox="0 -256 1792 1792">
+            <g transform="matrix(1,0,0,-1,68.338983,1217.0847)">
+              <path
+                  d="m 1611,320 q 0,-53 -37,-90 l -75,-75 q -38,-38 -91,-38 -54,0 -90,38 L 832,640 346,155 q -36,-38 -90,-38 -54,0 -90,38 l -75,75 q -38,36 -38,90 0,53 38,91 l 651,651 q 37,37 90,37 52,0 91,-37 l 650,-651 q 38,-38 38,-91 z"
+                  style="fill:#fff"/>
+            </g>
+          </svg>
+        </button>
+      </back-to-top>
+    </client-only>
   </div>
 </template>
 <script>
