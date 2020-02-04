677122eb4950e1304a74e45be81a52de0dd053fd
Wei Seng Tan <tanweiseng13@gmail.com>
2020-02-04 20:50:31 +0800

    clean(Nav): clean up

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,28 +1,32 @@
 <template>
   <nav v-sticky class="flex items-center justify-between flex-wrap bg-primary">
-    <div class="container flex flex-col md:flex-row font-bold text-white text-lg">
+    <div class="container flex flex-col lg:flex-row font-bold text-white text-lg">
       <div class="flex justify-between lg:hidden">
-        <div class="capitalize">
-          {{currentPage}}
-        </div>
+        <div class="capitalize">{{ currentPageName }}</div>
         <div>
-          <button id="hamburger"
-                  class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white self-end"
-                  @click="showMobileLinks = !showMobileLinks">
+          <button
+              id="hamburger"
+              class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white self-end"
+              @click="showMobileLinks = !showMobileLinks">
             <svg class="fill-current h-3 w-3" viewBox="0 0 20 20">
-              <title>Menu</title>
               <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
             </svg>
           </button>
         </div>
       </div>
       <div v-for="link in links" v-bind:key="link.name" class="hidden lg:block">
-        <nuxt-link class="mr-4 hover:text-green-200" :to="{name: link.name}">{{link.display}}
+        <nuxt-link class="mr-4 hover:text-green-200" :to="{ name: link.name }">
+          {{ link.display }}
         </nuxt-link>
       </div>
       <div v-for="link in links" v-bind:key="'mob-'+link.name" class="lg:hidden">
-        <nuxt-link v-if="showMobileLinks" class="mr-4 hover:text-green-200" :to="{name: link.name}"
-                   @click.native="showMobileLinks = false">{{link.display}}
+        <nuxt-link
+            v-if="showMobileLinks"
+            class="mr-4 hover:text-green-200"
+            :to="{ name: link.name }"
+            @click.native="showMobileLinks = false"
+        >
+          {{ link.display }}
         </nuxt-link>
       </div>
     </div>
@@ -33,8 +37,8 @@
   export default {
     name: "Nav",
     computed: {
-      currentPage: function() {
-        const currentLink =  this.links.find(link => link.name === this.$route.name);
+      currentPageName: function() {
+        const currentLink = this.links.find(link => link.name === this.$route.name);
         return currentLink && currentLink.display || '';
       }
     },
