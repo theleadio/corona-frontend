9b440eb8ada7c54152c2f1631f8f9c5eb16e3146
emperorinfinite <emperorpointguard@gmail.com>
2020-02-05 23:18:27 +0800

    fix news truncate

--- components/News.vue
+++ components/News.vue
@@ -2,20 +2,29 @@
   <a
     :href="article.url"
     target="_blank"
-    class="article flex w-full relative mb-4"
+    class="p-4 border-gray-400 border border-solid rounded flex w-full relative mb-4"
   >
     <div v-lazy:background-image="article.urlToImage" class="image bg-cover bg-center bg-no-repeat"></div>
     <div class="content w-full text-black">
-      <h2>{{ article.title | title(titleLength) }}</h2>
-      <p>{{ article.description | summary(summaryLength) }}</p>
+      <v-clamp autoresize :max-lines="3" class="text-base font-semibold leading-normal break-normal lg:text-xl mb-1">
+        {{ article.title }}
+      </v-clamp>
+      <v-clamp autoresize :max-lines="2" class="text-sm leading-snug break-normal text-gray-700 lg:text-base">
+        {{ article.description }}
+      </v-clamp>
       <div class="source-date lg:mt-3"><!--{{ article.publishedAt | moment('from') }}--></div>
     </div>
   </a>
 </template>
 
 <script>
+import VClamp from 'vue-clamp'
+
 export default {
   name: "News",
+  components: {
+    VClamp
+  },
   props: {
     titleLength: {
       type: Number,
@@ -28,6 +37,7 @@ export default {
       required: true
     }
   },
+  /*
   filters: {
     title(title, length) {
       title = title || '';
@@ -42,40 +52,30 @@ export default {
       return summary.substring(0, length) + "....";
     }
   }
+  */
 };
 </script>
 
 <style scoped>
+/*
 .article {
   padding: 20px 24px;
   border: 1px solid #E2E2E2;
   border-radius: 4px;
 }
-
+*/
 .image {
-  margin-right: 24px;
+  margin-right: 20px;
   width: 96px;
   height: 96px;
   flex: 0 0 96px;
   border-radius: 6px;
 }
-
+/*
 .content {
-  /*background: rgba(196, 196, 196, 0.7);*/
+  /*background: rgba(196, 196, 196, 0.7);
 }
-
-h2 {
-  font-weight: bold;
-  font-size: 20px;
-  line-height: 24px;
-}
-
-p {
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 20px;
-}
-
+*/
 .source-date {
   font-weight: 500;
   font-size: 12px;
