a055a5ad75bb9bf1c0914efb888d065245b3136b
Qi Xiang <Qixyqix@outlook.com>
2020-02-05 15:32:44 +0800

    Changed large shadows to smaller shadow

--- components/Analytics/Stats.vue
+++ components/Analytics/Stats.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-row justify-center">
-    <div class="max-w-sm w-3/4 rounded overflow-hidden shadow-lg text-center text-red-600">
+    <div class="w-1/3 rounded overflow-hidden shadow text-center text-red-600">
       <div class="px-6 py-1 pt-6 bg-red-100 text-xl lg:text-4xl font-bold font-sans">
         <template v-if="typeof confirmed === 'number'">
             <animated-number :value=confirmed :round="1" :duration="300" />
@@ -11,7 +11,7 @@
       </div>
       <div class="px-6 py-1 w-full bg-red-200 lg:text-xl font-semibold font-sans">Confirmed</div>
     </div>
-    <div class="max-w-sm w-3/4 mx-2 rounded overflow-hidden shadow-lg text-center text-green-600">
+    <div class="w-1/3 mx-2 rounded overflow-hidden shadow text-center text-green-600">
       <div class="px-6 py-1 pt-6 bg-green-100 text-xl lg:text-4xl font-bold font-sans">
         <template v-if="typeof recovered === 'number'">
             <animated-number :value=recovered :round="1" :duration="300" />
@@ -22,7 +22,7 @@
       </div>
       <div class="px-6 py-1 w-full bg-green-200 lg:text-xl font-semibold font-sans">Recovered</div>
     </div>
-    <div class="max-w-sm w-3/4 rounded overflow-hidden shadow-lg text-center text-gray-600">
+    <div class="w-1/3 rounded overflow-hidden shadow text-center text-gray-600">
       <div class="px-6 py-1 pt-6 bg-gray-200 text-xl lg:text-4xl font-bold font-sans">
         <template v-if="typeof deaths === 'number'">
             <animated-number :value=deaths :round="1" :duration="300" />
