eecef7a42a4da3d660d29c8bb6edd30524f80077
Akio <rsalunga29@gmail.com>
2020-02-07 03:27:54 +0800

    added vueapex charts as package and plugin; added outbreak trend and most affected region charts

--- components/Analytics/AffectedRegion.vue
+++ components/Analytics/AffectedRegion.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-    <p class="2xl font-bold">Affected region horizontal-stacked bar chart here</p>
+    <client-only placeholder="Loading...">
+      <apexcharts ref="chart" type="bar" width="100%" height="400px" :options="options" :series="series"></apexcharts>
+    </client-only>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
   props: {
     data: {
@@ -15,14 +19,102 @@ export default {
 
   data () {
     return {
-      chart: null,
+      options: {
+        title: {
+          text: 'Most affected regions',
+          align: 'left',
+        },
+
+        chart: {
+          id: 'affected-regions',
+          stacked: true,
+          toolbar: {
+            show: false,
+          },
+          zoom: {
+            enabled: false,
+          },
+        },
+
+        plotOptions: {
+          bar: {
+            horizontal: true,
+          },
+        },
+
+        legend: {
+          position: 'top',
+        },
+
+        tooltip: {
+          theme: 'light'
+        },
+
+        dataLabels: {
+          enabled: false,
+        },
+
+        stroke: {
+          width: 1,
+          colors: ['#fff'],
+        },
+
+        grid: {
+          row: {
+            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on
+            opacity: 0.5,
+          },
+        },
+
+        colors: ['#b7791f', '#c53030', '#38a169'], // yellow, red, green
+
+        xaxis: {
+          categories: [],
+          labels: {
+            formatter: function (val) {
+              return Math.abs(val) > 999 ? Math.sign(val)*((Math.abs(val)/1000).toFixed(1)) + 'k' : Math.sign(val)*Math.abs(val)
+            },
+          },
+        },
+      },
+
+      series: [],
     }
   },
 
-  methods: {
-    renderChart () {
-      //
+  watch: {
+    data (val) {
+      const categories = val.map(i => i.state)
+      const series = [
+        {
+          name: 'Total Confirmed Cases',
+          data: Array(categories.length).fill(0),
+        },
+        {
+          name: 'Total Mortality',
+          data: Array(categories.length).fill(0),
+        },
+        {
+          name: 'Total Recovered',
+          data: Array(categories.length).fill(0),
+        },
+      ]
+
+      this.$refs.chart.updateOptions({
+        xaxis: {
+          categories
+        }
+      })
+
+      val.map(i => i).forEach((item) => {
+        const categoryIdx = categories.indexOf(item.state)
+        series[0].data[categoryIdx] += item.total_confirmed
+        series[1].data[categoryIdx] += item.total_dead
+        series[2].data[categoryIdx] += item.total_recovered
+      })
+
+      this.$refs.chart.updateSeries(series)
     }
-  }
+  },
 }
 </script>
--- components/Analytics/OutbreakTrend.vue
+++ components/Analytics/OutbreakTrend.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-    <p class="2xl font-bold">Outbreak trend multi-line chart here</p>
+    <client-only placeholder="Loading...">
+      <apexcharts ref="chart" type="line" width="100%" height="400px" :options="options" :series="series"></apexcharts>
+    </client-only>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
   props: {
     data: {
@@ -15,14 +19,95 @@ export default {
 
   data () {
     return {
-      chart: null,
-    }
-  },
+      options: {
+        title: {
+          text: 'Outbreak trend over time',
+          align: 'left',
+        },
 
-  methods: {
-    renderChart () {
-      //
+        chart: {
+          id: 'outbreak-trend',
+          toolbar: {
+            show: false,
+          },
+          zoom: {
+            enabled: false,
+          },
+        },
+
+        legend: {
+          position: 'top',
+        },
+
+        tooltip: {
+          theme: 'light'
+        },
+
+        dataLabels: {
+          enabled: false,
+        },
+
+        stroke: {
+          curve: 'straight'
+        },
+
+        markers: {
+          size: 5,
+          strokeColor: '#111',
+          strokeWidth: 2,
+        },
+
+        grid: {
+          row: {
+            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on
+            opacity: 0.5,
+          },
+        },
+
+        colors: ['#b7791f', '#c53030', '#38a169'], // yellow, red, green
+
+        xaxis: {
+          categories: [],
+        },
+      },
+
+      series: [],
     }
-  }
+  },  
+
+  watch: {
+    data (val) {
+      const categories = val.map(i => moment(i.date_posted, 'YYYY-MM-DD').format('YYYY-MM-DD'))
+      const series = [
+        {
+          name: 'Total Confirmed Cases',
+          data: Array(categories.length).fill(0),
+        },
+        {
+          name: 'Total Mortality',
+          data: Array(categories.length).fill(0),
+        },
+        {
+          name: 'Total Recovered',
+          data: Array(categories.length).fill(0),
+        },
+      ]
+
+      this.$refs.chart.updateOptions({
+        xaxis: {
+          categories
+        }
+      })
+
+      val.map(i => i).forEach((item) => {
+        const categoryIdx = categories.indexOf(moment(item.date_posted, 'YYYY-MM-DD').format('YYYY-MM-DD'))
+        series[0].data[categoryIdx] += item.confirmed
+        series[1].data[categoryIdx] += item.dead
+        series[2].data[categoryIdx] += item.recovered
+      })
+
+      this.$refs.chart.updateSeries(series)
+    },
+  },
 }
 </script>
