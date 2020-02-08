0eb7ae96a912441bd60a777737fb2952b32ffb52
Wei Seng Tan <tanweiseng13@gmail.com>
2020-02-08 17:00:39 +0800

    style: fixes

--- pages/causes.vue
+++ pages/causes.vue
@@ -1,11 +1,11 @@
 <template>
   <main class="container">
-    <div class='w-full h-screen'>
+    <div class="w-full h-screen">
+      <h1 class="text-2xl font-bold mb-4">What is the 2019-nCov virus?</h1>
 
-      <div>What is the 2019 nCov virus?</div>
-
-      <iframe style='width:100%;height:500px;' src="https://www.youtube.com/embed/OZcRD9fV7jo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <iframe style='width:100%;height:100%' src='https://www.cdc.gov/coronavirus/2019-ncov/about/transmission.html'></iframe></div>
+      <iframe style="width:100%;height:500px;" src="https://www.youtube.com/embed/OZcRD9fV7jo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe style="width:100%;height:100%" src="https://www.cdc.gov/coronavirus/2019-ncov/about/transmission.html"></iframe>
+    </div>
   </main>
 </template>
 
--- pages/prevention.vue
+++ pages/prevention.vue
@@ -1,8 +1,12 @@
 <template>
   <main class="container">
-    <div class='w-full h-screen'>
-      <div>We will update this page with resources related to nCov prevention, here is a quick guide from <a class='hover:underline' href='https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public'>WHO</a>.</div>
-      <iframe style='width:100%;height:100%' src='https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public'></iframe></div>
+    <div class="w-full h-screen">
+      <div class="mb-4">
+        We will update this page with resources related to nCov prevention, here is a quick guide from <a class="hover:underline" href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public">WHO</a>.
+      </div>
+
+      <iframe style="width:100%;height:100%" src="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public"></iframe>
+    </div>
   </main>
 </template>
 <script>
