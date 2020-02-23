105ceffb18910d54c9c1f115631406c55f3cacb6
Wei Seng Tan <tanweiseng13@gmail.com>
2020-02-24 01:20:33 +0800

    fix: wording

--- pages/causes.vue
+++ pages/causes.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="container">
     <div class="w-full h-screen">
-      <h1 class="text-2xl font-bold mb-4">What is the COVID-19 virus?</h1>
+      <h1 class="text-2xl font-bold mb-4">What is COVID-19?</h1>
 
       <iframe style="width:100%;height:500px;" src="https://www.youtube.com/embed/OZcRD9fV7jo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <iframe style="width:100%;height:100%" src="https://www.cdc.gov/coronavirus/COVID-19/about/transmission.html"></iframe>
