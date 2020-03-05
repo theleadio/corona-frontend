c4b5ee9e391ec370acd9b9f24ce9de9123b7a4ec
Wei Seng Tan <tanweiseng13@gmail.com>
2020-03-05 21:17:12 +0800

    feat: implement stats page to be embeded in iframe

---
+++ pages/iframe/stats.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <logo class="lg:flex mb-4" />
+    <TopStats :limit="999" :show-title="false" />
+  </div>
+</template>
+<script>
+  import Logo from '~/components/Logo';
+  import TopStats from '~/components/TopStats';
+
+  export default {
+    layout: "blank",
+    components: {
+      Logo,
+      TopStats,
+    },
+  };
+</script>
+<style scoped>
+</style>
