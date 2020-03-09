28b24f9638ec5e7822b90ea75aec5dd645c3db89
Wei Seng Tan <tanweiseng13@gmail.com>
2020-03-09 23:30:47 +0800

    feat: add image for lazy load error

--- plugins/vue-lazyload.client.js
+++ plugins/vue-lazyload.client.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
 import VueLazyload from 'vue-lazyload';
 import LazyLoadSpinner from '~/assets/image/lazy-load-spinner.gif';
+import LazyLoadError from '~/assets/image/lazy-load-error.png';
 
 Vue.use(VueLazyload, {
   preLoad: 1.3,
-  error: LazyLoadSpinner,
+  error: LazyLoadError,
   loading: LazyLoadSpinner,
   attempt: 1,
   lazyComponent: true
