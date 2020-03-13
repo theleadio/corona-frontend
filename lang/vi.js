1e53b4a6329c56bd7963fcf0cccf0df370dd63f4
tjjone98 <tzero1898@gmail.com>
2020-03-13 20:50:50 +0700

    feat[language]: add vietnam language

---
+++ lang/vi.js
@@ -0,0 +1,103 @@
+export default {
+	menu: {
+		Home: 'Trang chủ',
+		'Travel Alert': 'Cảnh báo du lịch',
+		'What is COVID-19': 'COVID-19 là gì',
+		Prevention: 'Phòng ngừa',
+		Analytics: 'Phân tích',
+		About: 'Về chúng tôi'
+	},
+	Live: 'Trực tiếp',
+	'Stats Overview': 'Tổng quan thống kê',
+	Global: 'Toàn cầu',
+	Confirmed: 'Xác nhận',
+	Recovered: 'Phục hồi',
+	Deaths: 'Tử vong',
+	'more details': 'Chi tiết',
+	Search: 'Tìm kiếm',
+	'Verified News': 'Tin tức đã được xác minh',
+	'Clear All': 'Xoá hết',
+	'No result': 'Không kết quảt',
+	Prev: 'Trước',
+	Next: 'Tiếp theo',
+	'Countries Affected': 'Đất nước bị ảnh hưởng',
+	Source: 'Nguồn',
+	'and various international media':
+		'và các phương tiện truyền thông quốc tế khác nhau',
+	Country: 'Đất nước',
+	'Full list here': 'Toàn bộ danh sách',
+	'Cases identified on a cruise ship currently in Japanese territorial waters.':
+		'Các trường hợp được xác định trên một tàu du lịch hiện đang ở trong lãnh hải của Nhật Bản.',
+	Steps: 'Các bước',
+	'to prevent coronavirus': 'Để ngăn chặn coronavirus',
+	'Read and share this with others':
+		'Đọc và chia sẻ điều này với những người khác',
+	'Learn how': 'Học cách',
+	'Screening Centres': 'Trung tâm sàng lọc',
+	'Select Region': 'Chọn khu vực',
+	'Help us collate the locations for your country!':
+		'Hãy giúp chúng tôi đối chiều với quốc gia của bạn!',
+	'Join our Telegram group': 'Tham gia group Telegram của chúng tôi',
+	'GET IT ON': 'GET IT ON',
+
+	'Travel Alert Information': 'Thông tin cảnh báo du lịch',
+	'Select Country': 'Chọn quốc gia',
+
+	'What is COVID-19?': 'COVID-19 là gì?',
+
+	'We will update this page with resources related to nCov prevention, here is a quick guide from WHO.':
+		'Chúng tôi sẽ cập nhật với các bên liên quan đến phòng chống COVID-19, đây là hướng dẫn nhanh từ WHO.',
+
+	Overview: 'Tổng quan',
+	Reports: 'Báo cáo',
+	'Total Confirmed': 'Tổng số nhiễm bệnh',
+	'Total Recovered': 'Tổng số phục hồi',
+	'Total Deaths': 'Tổng số tử vong',
+	'Outbreak trend over time': 'Xu hướng bùng phát theo thời gian',
+	'Most affected regions': 'Khu vực bị ảnh hưởng nhiều nhất',
+
+	'About CoronaTracker': 'Mô tả CoronaTracker',
+	'CORONATRACKER.com is a community-based project powered by over 460 volunteers from across the globe, ranging from data scientists, medical professionals, UI/UX designers, fullstack developers, to the general public.':
+		'CORONATRACKER.com is a community-based project powered by over 460 volunteers from across the globe, ranging from data scientists, medical professionals, UI/UX designers, fullstack developers, to the general public.',
+	'It acts as a portal for the public to keep track of the latest news development about the COVID-19, alongside collecting data for further analysis. All the contents are handpicked, filtered, and curated by volunteers to our best extent to ensure that sources are reliable with minimal hoaxes and fake news, in the best benefit of the public. Natural Language Processing (NLP) is used to analyze trending content and identify meaningful topics over various news sources and social media platforms prior to being verified by our team.':
+		'It acts as a portal for the public to keep track of the latest news development about the COVID-19, alongside collecting data for further analysis. All the contents are handpicked, filtered, and curated by volunteers to our best extent to ensure that sources are reliable with minimal hoaxes and fake news, in the best benefit of the public. Natural Language Processing (NLP) is used to analyze trending content and identify meaningful topics over various news sources and social media platforms prior to being verified by our team.',
+	'CORONATRACKER.com also aims to gather data for in-depth analysis (e.g. time series to monitor the growth, spread of the virus) and localized news (e.g. news related to Malaysia, or KL only).The platform is vital for information consolidation, and all gathered information is open to use for the public.This is a 100% open source project, all information and publishable data are available on Github as well. Everyone is welcomed to contribute through our telegram channel.':
+		'CORONATRACKER.com also aims to gather data for in-depth analysis (e.g. time series to monitor the growth, spread of the virus) and localized news (e.g. news related to Malaysia, or KL only).The platform is vital for information consolidation, and all gathered information is open to use for the public.This is a 100% open source project, all information and publishable data are available on Github as well. Everyone is welcomed to contribute through our telegram channel.',
+	'Our team of data scientists use OSEMN framework to collect, clean, explore, model, and run their own analysis. For ease of public understanding, it is presented using graphic visualizations and dashboards. Through the data obtained, we hope to build machine learning models very soon.':
+		'Our team of data scientists use OSEMN framework to collect, clean, explore, model, and run their own analysis. For ease of public understanding, it is presented using graphic visualizations and dashboards. Through the data obtained, we hope to build machine learning models very soon.',
+	'We were inspired by the Coronavirus Outbreak Map by John Hopkins University that was very informative. We also recognised the difficulty to gather data for in-depth analysis through various other sources of scattered data on different news portals as well as in different languages.':
+		'We were inspired by the Coronavirus Outbreak Map by John Hopkins University that was very informative. We also recognised the difficulty to gather data for in-depth analysis through various other sources of scattered data on different news portals as well as in different languages.',
+	'Works started on the 25th of January, the first day of the Lunar New Year. Our team has been working relentlessly to make this a success, and launched officially on the 27th of January.':
+		'Works started on the 25th of January, the first day of the Lunar New Year. Our team has been working relentlessly to make this a success, and launched officially on the 27th of January.',
+	'We aim to use technology and make information more accessible, gather talented people all around the world to win the fight!':
+		'We aim to use technology and make information more accessible, gather talented people all around the world to win the fight!',
+	Sources: 'Sources',
+	'Here you can find a list of sources handpicked by the team.':
+		'Here you can find a list of sources handpicked by the team.',
+	Title: 'Title',
+	Description: 'Description',
+	'A report by Jasper Fuk-Woo Chan and colleagues, documented infections in health-care workers caring for patients with COVID-19 indicate human-to-human transmission and thus the risk of much wider spread of the disease.':
+		'A report by Jasper Fuk-Woo Chan and colleagues, documented infections in health-care workers caring for patients with COVID-19 indicate human-to-human transmission and thus the risk of much wider spread of the disease.',
+	'For the third time in as many decades, a zoonotic coronavirus has crossed species to infect human populations. This virus, provisionally called COVID-19, was first identified in Wuhan, China, in persons exposed to a seafood or wet market.':
+		'For the third time in as many decades, a zoonotic coronavirus has crossed species to infect human populations. This virus, provisionally called COVID-19, was first identified in Wuhan, China, in persons exposed to a seafood or wet market.',
+	'An online dashboard to visualize and track the reported cases on a daily timescale':
+		'An online dashboard to visualize and track the reported cases on a daily timescale',
+	'If you believe any of the source information above is inaccurate, please lodge a report using the following':
+		'If you believe any of the source information above is inaccurate, please lodge a report using the following',
+	form: 'form',
+	'Heroes behind': 'Anh hùng đứng đằng sau',
+	'Project Lead': 'Trưởng dự án',
+	'Lead Data Scientist': 'rưởng nhóm khoa học dữ liệut',
+	'Lead Full Stack Engineer': 'Trưởng nhóm full stack',
+	'Lead Mobile Developer': 'Trưởng nhóm phát triền di động',
+	'Lead Analyst': 'Trưởng nhóm chuyên viên phân tích',
+	'Head of Public Relation': 'Head of Public Relation',
+	'Head of Product': 'Đứng đầu sản phẩm',
+	'Lead Researcher': 'Trưởng nhóm nghiên cứu',
+	LinkedIn: 'LinkedIn',
+	'Team Analytics': 'Nhóm phân tích',
+	'Team Research': 'Nhóm nghiên cứu',
+	'Team Engineering': 'Nhóm kĩ sư',
+	'Team Design': 'Nhóm thiết kế',
+	'View more': 'Xem thêm'
+};
