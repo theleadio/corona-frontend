85887ceac137640415766f1fb3202788ab4baad3
Wei Seng Tan <tanweiseng13@gmail.com>
2020-03-16 21:31:55 +0800

    fix: add polyfill for Array.flat

--- utils/generateRoutes.js
+++ utils/generateRoutes.js
@@ -1,3 +1,11 @@
+Object.defineProperty(Array.prototype, 'flat', {
+  value: function(depth = 1) {
+    return this.reduce(function (flat, toFlatten) {
+      return flat.concat((Array.isArray(toFlatten) && (depth>1)) ? toFlatten.flat(depth-1) : toFlatten);
+    }, []);
+  }
+});
+
 export const generateRoutes = (locales, countries) => {
   const countryAliases = countries
     .map(a => a.urlAliases)
