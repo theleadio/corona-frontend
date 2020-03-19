b3302130bb08e53705e9e9cdae15d94da4d6fb5d
Samuel Lee <samueljklee@gmail.com>
2020-03-19 15:26:39 -0700

    fix: y-axis overlap and legend position

--- components/Country/BarChartNumber.vue
+++ components/Country/BarChartNumber.vue
@@ -56,11 +56,15 @@ export default {
           },
           stacked: true,
         },
+        grid: {
+          show: false,
+        },
         dataLabels: {
           enabled: false
         },
         yaxis: {
-          show: true
+          show: true,
+          opposite: true,
         },
         xaxis: {
           type: 'datetime',
@@ -68,9 +72,25 @@ export default {
           show: true
         },
         legend: {
-          position: 'left',
-          offsetX: -20,
-          offsetY: 125
+          position: 'bottom',
+          offsetX: 0,
+          offsetY: -5
+        },
+        tooltip: {
+          shared: true,
+          y: [{
+            formatter: function (y) {
+              return y;
+            }
+          }, {
+            formatter: function (y) {
+              return y;
+            }
+          }, {
+            formatter: function (y) {
+              return y;
+            }
+          }]
         },
       }
     };
