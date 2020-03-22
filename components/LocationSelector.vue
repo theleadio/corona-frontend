3d3c1da4f26da35802c4a800f34cecb99e75759a
Shirish Nigam <nigamshirish@gmail.com>
2020-03-22 11:40:28 +1000

    Fix: Overflow-x on opened location selector dropdown.

--- components/LocationSelector.vue
+++ components/LocationSelector.vue
@@ -35,7 +35,7 @@
             v-on-clickaway="closeOptions"
             />
         </div>
-        <ul class="absolute shadow text-gray-700 mt-10 z-50 w-full" v-if="optionsShowed" style="max-height: 36.3rem; overflow: auto;">
+        <ul class="absolute shadow text-gray-700 mt-10 z-50 w-full" v-if="optionsShowed" style="max-height: 36.3rem; overflow-y: auto; overflow-x:hidden">
           <li v-for="country in filteredCountries" :key="country.code">
             <a class="cursor-pointer bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
                @click="selectCountry(country)">
