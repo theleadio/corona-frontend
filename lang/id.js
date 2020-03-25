bff66278ca04da88fec15a018c4c096188a3452f
Muhammad Jawad Azzaki <42091841+sparagongf@users.noreply.github.com>
2020-03-25 20:39:02 +0700

    Update id.js

--- lang/id.js
+++ lang/id.js
@@ -8,12 +8,12 @@ export default {
     "About": "Tentang"
   },
   "Live": "Langsung",
-  "Stats Overview": "Ikhtisar Statistik",
+  "Stats Overview": "Statistik Secara Umum",
   "Global": "Global",
   "Confirmed": "Terkonfirmasi",
-  "Recovered": "Disembuhkan",
+  "Recovered": "Sembuh",
   "Deaths": "Meninggal",
-  "more details": "lebih lanjut",
+  "more details": "lebih detil",
   "Search": "Cari",
   "Verified News": "Berita yang disetujui",
   "Clear All": "Hapus Semua",
@@ -22,7 +22,7 @@ export default {
   "Next": "Selanjutnya",
   "Countries Affected": "Negara Terdampak",
   "Source": "Sumber",
-  "and various international media": "dan media internasional lainnya",
+  "and various international media": "dan berbagai media internasional",
   "Country": "Negara",
   "Full list here": "Lihat semua daftar",
   "Cases identified on a cruise ship currently in Japanese territorial waters.": "Kasus yang diidentifikasi di kapal pesiar saat ini di perairan teritorial Jepang.",
@@ -44,7 +44,7 @@ export default {
   "We will update this page with resources related to nCov prevention, here is a quick guide from": "Kami akan memperbarui halaman ini dengan sumber daya yang terkait dengan pencegahan nCov, berikut adalah panduan singkat dari",
   "WHO": "SIAPA",
 
-  "Overview": "Ikhtisar",
+  "Overview": "Gambaran Umum",
   "Reports": "Laporan",
   "Total Confirmed": "Total Terkonfirmasi",
   "Total Recovered": "Total Disembuhkan",
@@ -58,40 +58,40 @@ export default {
   "CORONATRACKER.com is a community-based project powered by over 460 volunteers from across the globe, ranging from data scientists, medical professionals, UI/UX designers, fullstack developers, to the general public.":
     "CORONATRACKER.com adalah proyek berbasis komunitas yang didukung oleh lebih dari 460 sukarelawan dari seluruh dunia, mulai dari ilmuwan data, profesional medis, desainer UI / UX, pengembang fullstack, hingga masyarakat umum.",
   "It acts as a portal for the public to keep track of the latest news development about the COVID-19, alongside collecting data for further analysis. All the contents are handpicked, filtered, and curated by volunteers to our best extent to ensure that sources are reliable with minimal hoaxes and fake news, in the best benefit of the public. Natural Language Processing (NLP) is used to analyze trending content and identify meaningful topics over various news sources and social media platforms prior to being verified by our team.":
-    "Ini bertindak sebagai portal bagi publik untuk melacak perkembangan berita terbaru tentang COVID-19, di samping mengumpulkan data untuk analisis lebih lanjut. Semua konten dipilih sendiri, disaring, dan dikuratori oleh sukarelawan sejauh mungkin untuk memastikan bahwa sumber dapat diandalkan dengan tipuan minimal dan berita palsu, dalam manfaat terbaik dari publik. Natural Language Processing (NLP) digunakan untuk menganalisis konten yang sedang tren dan mengidentifikasi topik yang bermakna atas berbagai sumber berita dan platform media sosial sebelum diverifikasi oleh tim kami. " ,
-  "CORONATRACKER.com also aims to gather data for in-depth analysis (e.g. time series to monitor the growth, spread of the virus) and localized news (e.g. news related to Malaysia, or KL only).The platform is vital for information consolidation, and all gathered information is open to use for the public.This is a 100% open source project, all information and publishable data are available on Github as well. Everyone is welcomed to contribute through our Telegram channel.":
-    "CORONATRACKER.com juga bertujuan untuk mengumpulkan data untuk analisis mendalam (mis. Rangkaian waktu untuk memantau pertumbuhan, penyebaran virus) dan berita lokal (mis. Berita yang terkait dengan Malaysia, atau hanya KL). Platform ini sangat penting untuk konsolidasi informasi , dan semua informasi yang dikumpulkan terbuka untuk digunakan untuk umum. Ini adalah proyek sumber terbuka 100%, semua informasi dan data yang dapat diterbitkan juga tersedia di Github. Semua orang dipersilakan untuk berkontribusi melalui saluran Telegram kami. ",
+    "Ini berperan sebagai pintu untuk masyarakat untuk melacak perkembangan berita terbaru tentang COVID-19, sekaligus mengumpulkan data untuk analisis lebih lanjut. Semua konten dipilih, disaring, dan diawasi oleh sukarelawan sebaik mungkin untuk memastikan bahwa sumber dapat dipercaya dengan minimal informasi dan berita palsu, untuk keuntungan publik. Natural Language Processing (NLP) digunakan untuk menganalisis konten yang sedang tren dan mengidentifikasi topik yang bermakna dari berbagai sumber berita dan platform media sosial sebelum diverifikasi oleh tim kami. " ,
+  "CORONATRACKER.com also aims to gather data for in-depth analysis (e.g. time series to monitor the growth, spread of the virus) and localized news (e.g. news related to Malaysia, or KL only).The platform is vital for information consolidation, and all gathered information is open to use for the public. This is a 100% open source project, all information and publishable data are available on Github as well. Everyone is welcomed to contribute through our Telegram channel.":
+    "CORONATRACKER.com juga bertujuan untuk mengumpulkan data untuk analisis mendalam (misalnya rangkaian waktu dalam memantau pertumbuhan, penyebaran virus) dan berita lokal (misalnya berita yang terkait dengan Malaysia, atau hanya KL). Platform ini sangat penting untuk konsolidasi informasi , dan semua informasi yang dikumpulkan terbuka untuk digunakan untuk publik. Ini adalah 100 % proyek terbuka , semua informasi dan data yang diterbitkan juga tersedia di Github. Semua orang dipersilakan untuk berkontribusi melalui channel Telegram kami. ",
   "Our team of data scientists use OSEMN framework to collect, clean, explore, model, and run their own analysis. For ease of public understanding, it is presented using graphic visualizations and dashboards. Through the data obtained, we hope to build machine learning models very soon.":
-    "Tim ilmuwan data kami menggunakan kerangka kerja OSEMN untuk mengumpulkan, membersihkan, mengeksplorasi, memodelkan, dan menjalankan analisis mereka sendiri. Untuk memudahkan pemahaman publik, disajikan dengan menggunakan visualisasi grafis dan dasbor. Melalui data yang diperoleh, kami berharap dapat membangun pembelajaran mesin model segera. ",
+    "Tim ilmuwan data kami menggunakan framework OSEMN untuk mengumpulkan, membersihkan, mengeksplorasi, memodelkan, dan menjalankan analisisnya sendiri. Untuk memudahkan pemahaman publik, framework ini disajikan dengan menggunakan visualisasi grafis dan beberapa instrumen. Melalui data yang diperoleh, kami berharap dapat membangun model machine learning segera. ",
   "We were inspired by the Coronavirus Outbreak Map by John Hopkins University that was very informative. We also recognised the difficulty to gather data for in-depth analysis through various other sources of scattered data on different news portals as well as in different languages.":
-    "Kami terinspirasi oleh Coronavirus Outbreak Map oleh Universitas John Hopkins yang sangat informatif. Kami juga mengenali kesulitan untuk mengumpulkan data untuk analisis mendalam melalui berbagai sumber lain data yang tersebar di portal berita yang berbeda serta dalam berbagai bahasa." ,
+    "Kami terinspirasi oleh Coronavirus Outbreak Map oleh Universitas John Hopkins yang sangat informatif. Kami juga memahami kesulitan dalam mengumpulkan data untuk analisis mendalam melalui berbagai sumber lain data yang tersebar di portal berita yang berbeda serta dalam berbagai bahasa." ,
   "Works started on the 25th of January, the first day of the Lunar New Year. Our team has been working relentlessly to make this a success, and launched officially on the 27th of January.":
-    "Pekerjaan dimulai pada tanggal 25 Januari, hari pertama Tahun Baru Imlek. Tim kami telah bekerja tanpa henti untuk menjadikan ini sukses, dan diluncurkan secara resmi pada tanggal 27 Januari.",
+    "Pekerjaan dimulai pada tanggal 25 Januari, hari pertama Tahun Baru Imlek. Tim kami telah bekerja tanpa henti untuk menjadikan ini suksesan, dan meluncurkan secara resmi pada tanggal 27 Januari.",
   "We aim to use technology and make information more accessible, gather talented people all around the world to win the fight!":
     "Kami bertujuan untuk menggunakan teknologi dan membuat informasi lebih mudah diakses, mengumpulkan orang-orang berbakat di seluruh dunia untuk memenangkan pertarungan!",
   "Sources": "Sumber",
   "Here you can find a list of sources handpicked by the team.":
-    "Di sini Anda dapat menemukan daftar sumber yang dipilih sendiri oleh tim.",
+    "Di sini Anda dapat menemukan daftar sumber yang dipilih oleh tim.",
   "Title": "Judul",
   "Description": "Deskripsi",
   "A report by Jasper Fuk-Woo Chan and colleagues, documented infections in health-care workers caring for patients with COVID-19 indicate human-to-human transmission and thus the risk of much wider spread of the disease.":
-    "Sebuah laporan oleh Jasper Fuk-Woo Chan dan rekannya, mendokumentasikan infeksi pada petugas layanan kesehatan yang merawat pasien dengan COVID-19 menunjukkan penularan dari manusia ke manusia dan dengan demikian risiko penyebaran penyakit yang jauh lebih luas.",
+    "Sebuah laporan oleh Jasper Fuk-Woo Chan dan rekannya, infeksi yang didokumentasikan oleh petugas layanan kesehatan yang merawat pasien dengan COVID-19 menunjukkan penularan dari manusia ke manusia dan risiko penyebaran penyakit yang jauh lebih luas.",
   "For the third time in as many decades, a zoonotic coronavirus has crossed species to infect human populations. This virus, provisionally called COVID-19, was first identified in Wuhan, China, in persons exposed to a seafood or wet market.":
     "Untuk ketiga kalinya dalam beberapa dekade, coronavirus zoonosis telah melintasi spesies untuk menginfeksi populasi manusia. Virus ini, sementara disebut COVID-19, pertama kali diidentifikasi di Wuhan, Cina, pada orang yang terpapar makanan laut atau pasar basah.",
   "An online dashboard to visualize and track the reported cases on a daily timescale":
-    "Dasbor online untuk memvisualisasikan dan melacak kasus yang dilaporkan pada skala waktu harian",
+    "Dashboard online untuk memvisualisasikan dan melacak kasus yang dilaporkan pada skala waktu harian",
   "If you believe any of the source information above is inaccurate, please lodge a report using the following":
-    "Jika Anda yakin ada salah satu sumber informasi di atas tidak akurat, silakan ajukan laporan menggunakan yang berikut",
+    "Jika Anda yakin ada salah satu sumber informasi di atas tidak akurat, silakan ajukan laporan dibawah sebagai berikut",
   "form": "Formulir",
-  "Heroes behind": "Pejuang di belakang layar",
+  "Heroes behind": "Pejuang dibelakangnya",
   "Project Lead": "Pimpinan Proyek",
-  "Lead Data Scientist": "Lead Data Scientist",
-  "Lead Full Stack Engineer": "Lead Full Stack Engineer",
-  "Lead Mobile Developer": "Lead Mobile Developer",
-  "Lead Analyst": "Lead Analyst",
+  "Lead Data Scientist": "Pimpinan Data Scientist",
+  "Lead Full Stack Engineer": "Pimpinan Full Stack Engineer",
+  "Lead Mobile Developer": "Pimpinan Mobile Developer",
+  "Lead Analyst": "Pimpinan Analyst",
   "Head of Public Relation": "Kepala Hubungan Masyarakat",
   "Head of Product": "Kepala Produk",
-  "Lead Researcher": "Peneliti Utama",
+  "Lead Researcher": "Kepala Peneliti",
   "LinkedIn": "LinkedIn",
   "Team Analytics": "Tim Analisis",
   "Team Research": "Tim Peneliti",
@@ -102,8 +102,8 @@ export default {
 
   "Fatality Rate": "Tingkat Fatalitas",
   "Positive Rate": "Tingkat Positif",
-  "OF TOTAL CASES": "OF TOTAL CASES",
-  "Time taken from confirmation to discharge": "Waktu yang diambil dari konfirmasi hingga keluar",
+  "OF TOTAL CASES": "DARI TOTAL KASUS",
+  "Time taken from confirmation to discharge": "Waktu yang dibutuhkan dari konfirmasi hingga dikeluarkan",
   "{number} days": "{number} hari",
   "+{number} since yesterday": "+{number} sejak kemarin",
   "Critical Cases treated in ICU": "Kasus Kritis dirawat di ICU",
@@ -114,9 +114,9 @@ export default {
   "Past 14 Days Chart": "Grafik 14 Hari Terakhir",
 
   "COVID-19 Corona Tracker": "COVID-19 Corona Tracker",
-  "COVID-19 Corona Tracker: The only independent World Health Organization (WHO) recognized one stop platform for verified data and news.": "COVID-19 Corona Tracker: Satu-satunya Organisasi Kesehatan Dunia (WHO) yang diakui mengakui platform satu atap untuk data dan berita yang terverifikasi.",
+  "COVID-19 Corona Tracker: The only independent World Health Organization (WHO) recognized one stop platform for verified data and news.": "COVID-19 Corona Tracker: Satu-satunya Organisasi Kesehatan Dunia (WHO) bebas yang diakui sebagai platform data dan berita yang terverifikasi.",
   "COVID-19 {country} Corona Tracker": "COVID-19 {country} Pelacak Corona",
-  "{country} COVID-19 Corona Tracker: The only independent World Health Organization (WHO) recognized one stop platform for verified data and news.": "{country} COVID-19 Corona Tracker: Satu-satunya Organisasi Kesehatan Dunia (WHO) yang independen mengakui platform satu atap untuk data dan berita yang terverifikasi.",
-  "COVID-19 related analytics, graphs, and charts": "COVID-19 analytics, grafik, dan bagan terkait",
+  "{country} COVID-19 Corona Tracker: The only independent World Health Organization (WHO) recognized one stop platform for verified data and news.": "{country} COVID-19 Corona Tracker: Satu-satunya Organisasi Kesehatan Dunia (WHO) bebas yang diakui sebagai platform data dan berita yang terverifikasi.",
+  "COVID-19 related analytics, graphs, and charts": "COVID-19 analitik dan grafik terkait",
   "Visual presentations of COVID-19 related data from verified sources such as WHO, CDC, ECDC, NHC of the PRC, JHU CSSE, DXY, QQ, and various international media.": "Presentasi visual dari data terkait COVID-19 dari sumber terverifikasi seperti WHO, CDC, ECDC, NHC PRC, JHU CSSE, DXY, QQ, dan berbagai media internasional.",
 };
