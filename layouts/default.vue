48a6c8f6e2d5e0893d32e664daec8bf449eb736a
Wei Seng Tan <tanweiseng13@gmail.com>
2020-03-25 18:48:22 +0800

    fix: remove buymeacoffee script from iframe page

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,6 +17,23 @@
         htmlAttrs: {
           lang: this.$i18n.locale,
         },
+        link: [
+          // This is needed for buymeacoffee widget
+          { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Cookie' }
+        ],
+        script: [
+          {
+            src: 'https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js',
+            'data-name': 'BMC-Widget',
+            'data-id': 'coronatracker',
+            'data-description': 'Support me on Buy me a coffee!',
+            'data-message': 'Thank you for visiting. You can now buy me a coffee!',
+            'data-color': '#FF813F',
+            'data-position': 'left',
+            'data-x_margin': '18',
+            'data-y_margin': '18',
+          }
+        ],
       }
     },
     components: {
