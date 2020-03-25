d62890369f2b7b153db5c33f083e8d13c0927eba
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2020-03-25 15:26:08 +0700

    Add based translation for indonesian language

---
+++ lang/id.js
@@ -0,0 +1,122 @@
+export default {
+  "menu": {
+    "Home": "Beranda",
+    "Travel Alert": "Peringatan Perjalanan",
+    "What is COVID-19": "Apa itu COVID-19",
+    "Prevention": "Pencegahan",
+    "Analytics": "Analitik",
+    "About": "Tentang"
+  },
+  "Live": "Langsung",
+  "Stats Overview": "Ikhtisar Statistik",
+  "Global": "Global",
+  "Confirmed": "Terkonfirmasi",
+  "Recovered": "Disembuhkan",
+  "Deaths": "Meninggal",
+  "more details": "lebih lanjut",
+  "Search": "Cari",
+  "Verified News": "Berita Terverifikasi",
+  "Clear All": "Hapus Semua",
+  "No result": "Tidak ada data",
+  "Prev": "Sebelumnya",
+  "Next": "Selanjutnya",
+  "Countries Affected": "Negara Terdampak",
+  "Source": "Sumber",
+  "and various international media": "dan media international lainnya",
+  "Country": "Negara",
+  "Full list here": "Lihat semua daftar",
+  "Cases identified on a cruise ship currently in Japanese territorial waters.": "Cases identified on a cruise ship currently in Japanese territorial waters.",
+  "Steps": "Langkah-langkah",
+  "to prevent coronavirus": "untuk mencegah coronavirus",
+  "Read and share this with others": "Baca dan bagikan kepada yang lainnya",
+  "Learn how": "Pelajari lebih lanjut",
+  "Screening Centres": "Screening Centres",
+  "Select Region": "Pilih daerah",
+  "Help us collate the locations for your country!": "Bantu kami menyusun lokasi untuk negara Anda!",
+  "Join our Telegram group": "Bergabung dengan grup Telegram kami",
+  "GET IT ON": "DAPATKAN SEKARANG",
+
+  "Travel Alert Information": "Informasi Peringatan Perjalanan",
+  "Select Country": "Pilih Negara",
+
+  "What is COVID-19?": "Apa itu COVID-19?",
+
+  "We will update this page with resources related to nCov prevention, here is a quick guide from": "We will update this page with resources related to nCov prevention, here is a quick guide from",
+  "WHO": "WHO",
+
+  "Overview": "Ikhtisar",
+  "Reports": "Laporan",
+  "Total Confirmed": "Total Terkonfirmasi",
+  "Total Recovered": "Total Disembuhkan",
+  "Total Deaths": "Total Meninggal",
+  "Outbreak trend over time": "Outbreak trend over time",
+  "Top countries with daily new cases": "Negara dengan kasus harian tertinggi",
+  "Daily New Cases": "Kasus baru setiap hari",
+  "Daily New Deaths": "Kematian baru setiap hari",
+
+  "About CoronaTracker": "Tentang CoronaTracker",
+  "CORONATRACKER.com is a community-based project powered by over 460 volunteers from across the globe, ranging from data scientists, medical professionals, UI/UX designers, fullstack developers, to the general public.":
+    "CORONATRACKER.com is a community-based project powered by over 460 volunteers from across the globe, ranging from data scientists, medical professionals, UI/UX designers, fullstack developers, to the general public.",
+  "It acts as a portal for the public to keep track of the latest news development about the COVID-19, alongside collecting data for further analysis. All the contents are handpicked, filtered, and curated by volunteers to our best extent to ensure that sources are reliable with minimal hoaxes and fake news, in the best benefit of the public. Natural Language Processing (NLP) is used to analyze trending content and identify meaningful topics over various news sources and social media platforms prior to being verified by our team.":
+    "It acts as a portal for the public to keep track of the latest news development about the COVID-19, alongside collecting data for further analysis. All the contents are handpicked, filtered, and curated by volunteers to our best extent to ensure that sources are reliable with minimal hoaxes and fake news, in the best benefit of the public. Natural Language Processing (NLP) is used to analyze trending content and identify meaningful topics over various news sources and social media platforms prior to being verified by our team.",
+  "CORONATRACKER.com also aims to gather data for in-depth analysis (e.g. time series to monitor the growth, spread of the virus) and localized news (e.g. news related to Malaysia, or KL only).The platform is vital for information consolidation, and all gathered information is open to use for the public.This is a 100% open source project, all information and publishable data are available on Github as well. Everyone is welcomed to contribute through our Telegram channel.":
+    "CORONATRACKER.com also aims to gather data for in-depth analysis (e.g. time series to monitor the growth, spread of the virus) and localized news (e.g. news related to Malaysia, or KL only).The platform is vital for information consolidation, and all gathered information is open to use for the public.This is a 100% open source project, all information and publishable data are available on Github as well. Everyone is welcomed to contribute through our Telegram channel.",
+  "Our team of data scientists use OSEMN framework to collect, clean, explore, model, and run their own analysis. For ease of public understanding, it is presented using graphic visualizations and dashboards. Through the data obtained, we hope to build machine learning models very soon.":
+    "Our team of data scientists use OSEMN framework to collect, clean, explore, model, and run their own analysis. For ease of public understanding, it is presented using graphic visualizations and dashboards. Through the data obtained, we hope to build machine learning models very soon.",
+  "We were inspired by the Coronavirus Outbreak Map by John Hopkins University that was very informative. We also recognised the difficulty to gather data for in-depth analysis through various other sources of scattered data on different news portals as well as in different languages.":
+    "We were inspired by the Coronavirus Outbreak Map by John Hopkins University that was very informative. We also recognised the difficulty to gather data for in-depth analysis through various other sources of scattered data on different news portals as well as in different languages.",
+  "Works started on the 25th of January, the first day of the Lunar New Year. Our team has been working relentlessly to make this a success, and launched officially on the 27th of January.":
+    "Works started on the 25th of January, the first day of the Lunar New Year. Our team has been working relentlessly to make this a success, and launched officially on the 27th of January.",
+  "We aim to use technology and make information more accessible, gather talented people all around the world to win the fight!":
+    "We aim to use technology and make information more accessible, gather talented people all around the world to win the fight!",
+  "Sources": "Sumber",
+  "Here you can find a list of sources handpicked by the team.":
+    "Here you can find a list of sources handpicked by the team.",
+  "Title": "Judul",
+  "Description": "Deskripsi",
+  "A report by Jasper Fuk-Woo Chan and colleagues, documented infections in health-care workers caring for patients with COVID-19 indicate human-to-human transmission and thus the risk of much wider spread of the disease.":
+    "A report by Jasper Fuk-Woo Chan and colleagues, documented infections in health-care workers caring for patients with COVID-19 indicate human-to-human transmission and thus the risk of much wider spread of the disease.",
+  "For the third time in as many decades, a zoonotic coronavirus has crossed species to infect human populations. This virus, provisionally called COVID-19, was first identified in Wuhan, China, in persons exposed to a seafood or wet market.":
+    "For the third time in as many decades, a zoonotic coronavirus has crossed species to infect human populations. This virus, provisionally called COVID-19, was first identified in Wuhan, China, in persons exposed to a seafood or wet market.",
+  "An online dashboard to visualize and track the reported cases on a daily timescale":
+    "An online dashboard to visualize and track the reported cases on a daily timescale",
+  "If you believe any of the source information above is inaccurate, please lodge a report using the following":
+    "If you believe any of the source information above is inaccurate, please lodge a report using the following",
+  "form": "formulir",
+  "Heroes behind": "Pejuang di belakang layar",
+  "Project Lead": "Project Lead",
+  "Lead Data Scientist": "Lead Data Scientist",
+  "Lead Full Stack Engineer": "Lead Full Stack Engineer",
+  "Lead Mobile Developer": "Lead Mobile Developer",
+  "Lead Analyst": "Lead Analyst",
+  "Head of Public Relation": "Head of Public Relations",
+  "Head of Product": "Head of Product",
+  "Lead Researcher": "Lead Researcher",
+  "LinkedIn": "LinkedIn",
+  "Team Analytics": "Team Analytics",
+  "Team Research": "Team Research",
+  "Team Engineering": "Team Engineering",
+  "Team Design": "Team Design",
+  "Team Public Relations": "Team Public Relations",
+  "View more": "Lihat lebih banyak",
+
+  "Fatality Rate": "Fatality Rate",
+  "Positive Rate": "Positive Rate",
+  "OF TOTAL CASES": "OF TOTAL CASES",
+  "Time taken from confirmation to discharge": "Time taken from confirmation to discharge",
+  "{number} days": "{number} days",
+  "+{number} since yesterday": "+{number} since yesterday",
+  "Critical Cases treated in ICU": "Critical Cases treated in ICU",
+  "of total cases": "of total cases",
+  "Daily Cases Receiving Treatment": "Daily Cases Receiving Treatment",
+  "Daily Confirmed Cases": "Daily Confirmed Cases",
+  "Per Million Population": "Per Million Population",
+  "Past 14 Days Chart": "Past 14 Days Chart",
+
+  "COVID-19 Corona Tracker": "COVID-19 Corona Tracker",
+  "COVID-19 Corona Tracker: The only independent World Health Organization (WHO) recognized one stop platform for verified data and news.": "COVID-19 Corona Tracker: The only independent World Health Organization (WHO) recognized one stop platform for verified data and news.",
+  "COVID-19 {country} Corona Tracker": "COVID-19 {country} Corona Tracker",
+  "{country} COVID-19 Corona Tracker: The only independent World Health Organization (WHO) recognized one stop platform for verified data and news.": "{country} COVID-19 Corona Tracker: The only independent World Health Organization (WHO) recognized one stop platform for verified data and news.",
+  "COVID-19 related analytics, graphs, and charts": "COVID-19 related analytics, graphs, and charts",
+  "Visual presentations of COVID-19 related data from verified sources such as WHO, CDC, ECDC, NHC of the PRC, JHU CSSE, DXY, QQ, and various international media.": "Visual presentations of COVID-19 related data from verified sources such as WHO, CDC, ECDC, NHC of the PRC, JHU CSSE, DXY, QQ, and various international media.",
+};
