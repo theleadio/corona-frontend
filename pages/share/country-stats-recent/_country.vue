1c773d163351b91f4cd1fffb25c24463e457ff7d
Derrick Boden <derrick@omg-productions.com>
2020-03-28 13:51:56 -0400

    Adding three sharing routes, w/ view stubs.

---
+++ pages/share/country-stats-recent/_country.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    TODO
+  </div>
+</template>
+<script>
+  import Flag from '~/components/Flag';
+  import Logo from '~/components/Logo';
+  import Stats from '~/components/Analytics/Stats';
+
+  export default {
+    name: "ShareCountryStatsRecent",
+    layout: "blank",
+    components: {
+      Flag,
+      Logo,
+      Stats
+    },
+    data: function() {
+      return {};
+    },
+    computed: {
+      country() {
+        const countryToFind = this.$route.params.country;
+        const countryEntry = COUNTRIES.find(country => country.urlAliases.includes(countryToFind));
+        return countryEntry || {}
+      },
+      countryCode() {
+        const countryToFind = this.$route.params.country;
+        const countryEntry = COUNTRIES.find(country => country.urlAliases.includes(countryToFind));
+        return countryEntry?.code
+      }
+    },
+    methods: {
+      loadStats() {
+        //
+      },
+    },
+    created() {
+      this.loadStats();
+    },
+  };
+</script>
+<style scoped>
+</style>
---
+++ pages/share/global-stats-today.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    TODO
+  </div>
+</template>
+<script>
+  import Flag from '~/components/Flag';
+  import Logo from '~/components/Logo';
+  import Stats from '~/components/Analytics/Stats';
+
+  export default {
+    name: "ShareGlobalStatsToday",
+    layout: "blank",
+    components: {
+      Flag,
+      Logo,
+      Stats
+    },
+    data: function() {
+      return {};
+    },
+    methods: {
+      loadStats() {
+        //
+      },
+    },
+    created() {
+      this.loadStats();
+    },
+  };
+</script>
+<style scoped>
+</style>
