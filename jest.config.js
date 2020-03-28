7bed130e00e2895eefde784833d3aceb102bd689
Victor Correa <victorac.public@gmail.com>
2020-03-27 23:36:15 -0300

    Adding ability to run unit tests

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,8 @@ module.exports = {
   moduleFileExtensions: ['js', 'vue', 'json'],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    '.*\\.(vue)$': 'vue-jest',
+    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub"
   },
   collectCoverage: true,
   collectCoverageFrom: [
