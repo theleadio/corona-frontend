09b408efcc243331c0e0c6970fdf407a00a5667d
Wei Seng Tan <tanweiseng13@gmail.com>
2020-03-28 15:43:07 +0800

    chore: remove deprecated analytics api

--- api/analytics.js
+++ api/analytics.js
@@ -1,28 +1,13 @@
 export default axios => ({
-  fetchTrendByDate: (start_date, end_date) => {
-    return axios.get(`/analytics/trend?start_date=${start_date}&end_date=${end_date}`)
-      .then(res => res.data)
-  },
-
   fetchTopTrendingCases: (limit) => {
     const limitQuery = limit ? `?limit=${limit}` : ``
     return axios.get(`/v3/stats/worldometer/totalTrendingCases` + limitQuery)
       .then(res => res.data)
   },
   
-  fetchAffectedRegion: () => {
-    return axios.get(`/analytics/area`)
-      .then(res => res.data)
-  },
-
   // deprecating
   fetchAffectedCountry: () => {
     return axios.get(`/v2/analytics/country`)
-      .then(res => res.data)
-  },
-
-  fetchAllAffectedCountry: () => {
-    return axios.get(`/v3/stats/worldometer/country`)
       .then(res => res.data)
   },
 
