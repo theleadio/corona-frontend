41316932c6a0326108909c0dd74ff23b494ced07
Wei Seng Tan <tanweiseng13@gmail.com>
2020-03-28 15:40:50 +0800

    chore: remove deprecated api and page

--- api/stats.js
+++ api/stats.js
@@ -1,9 +1,4 @@
 export default axios => ({
-  getStats: (countryCode) => {
-    return axios.get(`/v3/stats/bno?countryCode=${countryCode}`)
-      .then(res => res.data);
-  },
-  
   getTopNCountryStats: (limit) => {
     const limitQuery = limit ? `?limit=${limit}` : ``
     return axios.get('/v3/stats/worldometer/topCountry' + limitQuery)
@@ -18,31 +13,6 @@ export default axios => ({
   getGlobalStats: () => {
     return axios.get(`/v3/stats/worldometer/global`)
       .then(res => res.data);
-  },
-
-  getLatestStats: () => {
-    return axios.get(`/stats/latest`)
-      .then(res => res.data);
-  },
-
-  getTopStats: (limit) => {
-    return axios.get(`/v2/stats/top?limit=${limit}`)
-      .then(res => res.data);
-  },
-
-  getTotalCasesByCountry: (countryCode) => {
-    return axios.get(`/v3/stats/bno/total_daily_cases/country?countryCode=${countryCode}`)
-      .then(res => res.data);
-  },
-
-  getDailyCasesByCountry: (countryCode) => {
-    return axios.get(`/v3/stats/bno/daily_cases/country?countryCode=${countryCode}`)
-    .then(res => res.data);
-  },
-
-  getTrendByCountry: (countryCode, startDate, endDate) => {
-    return axios.get(`/analytics/trend/country?country_code=${countryCode}&start_date=${startDate}&end_date=${endDate}`)
-    .then(res => res.data);
   },
 
   getTrendByCountryDate: (countryCode, startDate, endDate) => {
