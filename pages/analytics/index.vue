7d68de6d88a627b32160533959754b455124b01f
Wei Seng Tan <tanweiseng13@gmail.com>
2020-03-31 00:48:12 +0800

    fix: add missing hid

--- pages/analytics/index.vue
+++ pages/analytics/index.vue
@@ -114,8 +114,8 @@ export default {
         { hid: 'og-description', property: 'og:title', content: description },
         { hid: 'twitter-title', property: 'twitter:title', content: title },
         { hid: 'twitter-description', property: 'twitter:title', content: description },
-        { property: 'og:image', content: process.env.API_BASE_URL + '/share/globalStatsToday' },
-        { property: 'twitter-image', content: process.env.API_BASE_URL + '/share/globalStatsToday' }
+        { hid: 'og-image', property: 'og:image', content: process.env.API_BASE_URL + '/share/globalStatsToday' },
+        { hid: 'twitter-image', property: 'twitter-image', content: process.env.API_BASE_URL + '/share/globalStatsToday' }
       ],
     };
   },
