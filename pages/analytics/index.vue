2b350d6540885a14345ca317a480196cc3d10284
Derrick Boden <derrick@omg-productions.com>
2020-03-31 08:24:13 -0400

    Added process browser check for nuxt generation.

--- pages/analytics/index.vue
+++ pages/analytics/index.vue
@@ -124,7 +124,7 @@ export default {
 
   computed: {
     shareUrl() {
-      return process.browser ? window.location.href : 'https://www.coronatracker.com';
+      return process.browser ? window.location.href : 'https://www.coronatracker.com/analytics';
     }
   },
 
