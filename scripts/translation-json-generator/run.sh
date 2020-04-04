22d542de9454b5afb6dac2a007f44507902f0c16
Wei Seng Tan <tanweiseng13@gmail.com>
2020-04-04 21:57:16 +0800

    feat: add new languages to generate lang script

--- scripts/translation-json-generator/run.sh
+++ scripts/translation-json-generator/run.sh
@@ -13,29 +13,33 @@ python3 lang-generator.py -c d ms.js \
 && mv id.js ../../lang \
 && python3 lang-generator.py -c k nl.js \
 && mv nl.js ../../lang \
-&& python3 lang-generator.py -c l sv.js \
+&& python3 lang-generator.py -c k sv.js \
 && mv sv.js ../../lang \
-&& python3 lang-generator.py -c m tl.js \
+&& python3 lang-generator.py -c l tl.js \
 && mv tl.js ../../lang \
-&& python3 lang-generator.py -c n es-mx.js \
+&& python3 lang-generator.py -c m es-mx.js \
 && mv es-mx.js ../../lang \
-&& python3 lang-generator.py -c o de.js \
+&& python3 lang-generator.py -c n de.js \
 && mv de.js ../../lang \
-&& python3 lang-generator.py -c p my.js \
+&& python3 lang-generator.py -c o my.js \
 && mv my.js ../../lang \
-&& python3 lang-generator.py -c q pl.js \
+&& python3 lang-generator.py -c p pl.js \
 && mv pl.js ../../lang \
-&& python3 lang-generator.py -c r it.js \
+&& python3 lang-generator.py -c q it.js \
 && mv it.js ../../lang \
-&& python3 lang-generator.py -c s hi.js \
+&& python3 lang-generator.py -c r hi.js \
 && mv hi.js ../../lang \
-&& python3 lang-generator.py -c t ur.js \
+&& python3 lang-generator.py -c s ur.js \
 && mv ur.js ../../lang \
-&& python3 lang-generator.py -c u vi.js \
+&& python3 lang-generator.py -c t vi.js \
 && mv vi.js ../../lang \
-&& python3 lang-generator.py -c v pt-br.js \
+&& python3 lang-generator.py -c u pt-br.js \
 && mv pt-br.js ../../lang \
-&& python3 lang-generator.py -c w ta.js \
+&& python3 lang-generator.py -c v ta.js \
 && mv ta.js ../../lang \
-&& python3 lang-generator.py -c x fr.js \
+&& python3 lang-generator.py -c w fr.js \
 && mv fr.js ../../lang \
+&& python3 lang-generator.py -c x ilo.js \
+&& mv ilo.js ../../lang \
+&& python3 lang-generator.py -c y bik.js \
+&& mv bik.js ../../lang \
